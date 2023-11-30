115eef87be29cb8a66d6fd011a5fb2ca6f030d13
Phil Miesle <phil.miesle@datastax.com>
2023-11-30 15:09:50 +0000

    adding click and range/cuisine filtering

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,9 @@ var default_zoom = 16;
 var default_lat = 53.3416362; 
 var default_lon = -6.2627662;
 
+var currentMarkers = [];
+var allRestaurantMarkers = [];
+
 var map = L.map('map').setView([default_lat, default_lon], default_zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
@@ -39,4 +42,113 @@ function searchLocation() {
             });
     }
 }
+
+map.on('click', function(e) {
+    currentMarkers.forEach(function(marker) {
+        map.removeLayer(marker);
+    });
+    currentMarkers = [];
+
+    var lat = e.latlng.lat;
+    var lon = e.latlng.lng;
+    fetchRestaurants(lat, lon);
+});
+
+function updateRadiusValue(value) {
+    document.getElementById('radiusValue').textContent = value;
+}
+
+function fetchRestaurants(lat, lon) {
+    var radius = document.getElementById('radiusSlider').value;
+
+    var query = `
+        [out:json];
+        (
+          node["amenity"="restaurant"](around:${radius},${lat},${lon});
+          way["amenity"="restaurant"](around:${radius},${lat},${lon});
+          rel["amenity"="restaurant"](around:${radius},${lat},${lon});
+        );
+        out center;
+    `;
+
+    fetch(`https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`)
+        .then(response => response.json())
+        .then(data => {
+            // Process and display the data
+            displayRestaurants(data);
+        })
+        .catch(error => {
+            console.error('Error fetching restaurants:', error);
+        });
+}
+
+function displayRestaurants(data) {
+    var cuisineSet = new Set(); // To store unique cuisines
+
+    // Clear existing markers
+    clearMarkers();
+
+    data.elements.forEach(function(element) {
+        var lat = element.lat || element.center.lat;
+        var lon = element.lon || element.center.lon;
+        var name = element.tags.name || 'Restaurant';
+        var cuisine = element.tags.cuisine || 'Unknown';
+
+        cuisineSet.add(cuisine);
+
+        // Create and add marker
+        var marker = L.marker([lat, lon], { icon: greenIcon }).addTo(map)
+            .bindPopup(`${name}<br>Cuisine: ${cuisine}`);
+        marker.cuisine = cuisine;
+        allRestaurantMarkers.push(marker);
+    });
+
+    // Populate the cuisine selector
+    populateCuisineSelector(Array.from(cuisineSet));
+}
+
+function populateCuisineSelector(cuisines) {
+    var select = document.getElementById('cuisineSelect');
+    select.innerHTML = '<option value="">Select Cuisine</option>'; // Reset options
+
+    cuisines.forEach(function(cuisine) {
+        var option = document.createElement('option');
+        option.value = option.textContent = cuisine;
+        select.appendChild(option);
+    });
+}
+
+function filterCuisine() {
+    var selectedCuisine = document.getElementById('cuisineSelect').value;
+    allRestaurantMarkers.forEach(function(marker) {
+        if (selectedCuisine === "" || marker.cuisine === selectedCuisine) {
+            marker.setIcon(greenIcon);
+            marker.setOpacity(1);
+        } else {
+            marker.setIcon(greyIcon);
+            marker.setOpacity(0.5);
+        }
+    });
+}
+
+function clearMarkers() {
+    allRestaurantMarkers.forEach(function(marker) {
+        map.removeLayer(marker);
+    });
+    allRestaurantMarkers = [];
+}
+
+
+var greenIcon = L.divIcon({
+    className: 'green-marker',
+    html: '<span class="material-symbols-outlined">location_on</span>',
+    iconSize: [48, 48],
+    iconAnchor: [12, 24]
+});
+
+var greyIcon = L.divIcon({
+    className: 'grey-marker',
+    html: '<span class="material-symbols-outlined">location_on</span>',
+    iconAnchor: [12, 24]
+});
 
